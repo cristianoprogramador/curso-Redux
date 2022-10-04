80cbaefc834a93ce3158b5eeea17b5c9219bbe89
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-10-04 17:38:44 -0300

    Primeira versao do carrinho com o Redux

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import api from '../services/api'
+import { useDispatch } from 'react-redux'
 import { IProduct } from '../store/modules/cart/types'
+import { addProductToCart } from '../store/modules/cart/actions'
 
 const Catalog: React.FC = () => {
+    const dispatch = useDispatch()
     const [catalog, setCatalog] = useState<IProduct[]>([])
 
     useEffect(() => {
@@ -10,6 +13,14 @@ const Catalog: React.FC = () => {
             setCatalog(response.data)
         })
     }, [])
+
+    const handleAddProductToCart = useCallback(
+        (product: IProduct) => {
+            dispatch(addProductToCart(product))
+        },
+        [dispatch],
+    )
+
 
     return (
         <main>
@@ -20,7 +31,12 @@ const Catalog: React.FC = () => {
                     <strong>{product.title}</strong> {' - '}
                     <span>{product.price}</span>{'  '}
 
-                    <button type='button'>Comprar</button>
+                    <button
+                        type='button'
+                        onClick={() => handleAddProductToCart(product)}
+                    >
+                        Comprar
+                    </button>
                 </article>
             ))}
         </main>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,10 @@
 import { legacy_createStore as createStore} from 'redux'
+import { ICartState } from './modules/cart/types'
 import rootReducer from './modules/rootReducer'
+
+export interface IState {
+  cart: ICartState
+}
 
 const store = createStore(rootReducer)
 
